41d4b63da742b50311b4220a9031e7f3b6abae8c
rpdecks <rpdecks@gmail.com>
2020-05-16 15:17:37 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -16,19 +16,34 @@ function getListOfBooks(){
 function listTheBooks(response){
     // books = Array.from(response)
     parent = document.getElementById('list')
-    // parent.addEventListener('click', expandBook)
+    parent.addEventListener('click', expandBook)
     response.forEach(function(book){
         bookLi = document.createElement('li')
-        bookLi.addEventListener('click', expandBook)
+        // bookLi.addEventListener('click', expandBook)
         bookLi.innerText = book.title
-        bookLi.img = book.img
+        bookLi.dataset.img_url = book.img_url
+        bookLi.dataset.title = book.title
+        bookLi.dataset.desc = book.description
+        // bookLi.dataset.users = Array.from(book.users)
+        bookLi.dataset.users = JSON.stringify(book.users)
+        // debugger;
         parent.appendChild(bookLi)
     })
 }
 
 function expandBook(book){
     parent = document.getElementById('show-panel')
-    // parent.children.remove
-    debugger;
-    // bookImg = book.img
+    title = document.createElement('h3')
+    title.innerText = book.target.dataset.title
+    book_img = document.createElement('img')
+    book_img.src = book.target.dataset.img_url
+    desc = document.createElement('p')
+    desc.innerText = book.target.dataset.desc
+    parent.append(title, book_img, desc)
+    users = JSON.parse(book.target.dataset.users)
+    users.forEach(function(user){
+        userLi = document.createElement('li')
+        userLi.innerText = user.username
+        parent.appendChild(userLi)
+    })
 }
