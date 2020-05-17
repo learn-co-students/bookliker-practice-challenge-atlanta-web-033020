bc4d29c3333ef8b6d306ae2362b445550cfb39c7
rpdecks <rpdecks@gmail.com>
2020-05-17 11:17:13 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -14,36 +14,49 @@ function getListOfBooks(){
 }
 
 function listTheBooks(response){
-    // books = Array.from(response)
     parent = document.getElementById('list')
-    parent.addEventListener('click', expandBook)
     response.forEach(function(book){
         bookLi = document.createElement('li')
-        // bookLi.addEventListener('click', expandBook)
         bookLi.innerText = book.title
-        bookLi.dataset.img_url = book.img_url
-        bookLi.dataset.title = book.title
-        bookLi.dataset.desc = book.description
-        // bookLi.dataset.users = Array.from(book.users)
-        bookLi.dataset.users = JSON.stringify(book.users)
-        // debugger;
+        bookLi.addEventListener('click', (event) => {
+            expandBook(book)
+        })
         parent.appendChild(bookLi)
     })
 }
 
-function expandBook(book){
-    parent = document.getElementById('show-panel')
-    title = document.createElement('h3')
-    title.innerText = book.target.dataset.title
-    book_img = document.createElement('img')
-    book_img.src = book.target.dataset.img_url
-    desc = document.createElement('p')
-    desc.innerText = book.target.dataset.desc
+function expandBook(book) {
+    let parent = document.getElementById('show-panel')
+    let title = document.createElement('h3')
+    title.innerText = book.title
+    let book_img = document.createElement('img')
+    book_img.src = book.img_url
+    let desc = document.createElement('p')
+    desc.innerText = book.description
     parent.append(title, book_img, desc)
-    users = JSON.parse(book.target.dataset.users)
+    let users = book.users;
     users.forEach(function(user){
-        userLi = document.createElement('li')
-        userLi.innerText = user.username
-        parent.appendChild(userLi)
+        userUl = document.createElement('div')
+        userUl.innerText = user.username
+        parent.appendChild(userUl)
     })
+    // create and add like button to page with listener for click
+    likeBtn = document.createElement('btn')
+    likeBtn.addEventListener('click', (event) => {
+        likeBook(book)
+    })
+    likeBtn.innerText = 'Like'
+    likeBtn.className = 'button'
+    parent.appendChild(likeBtn)
+}
+
+function likeBook(book){
+    fetch('http://localhost:3000/books/id')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response){
+        listTheBooks(response)
+    })
+    console.log(book)
 }
